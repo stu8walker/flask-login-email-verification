1ca194d59a33e5b8d264b5f6d4497aa1cf5c2f65
Stuart Walker <stuartwalker485@gmail.com>
2020-02-01 14:12:31 +0000

    Flask-Mail config added

--- config.py
+++ config.py
@@ -6,13 +6,27 @@ load_dotenv(os.path.join(basedir, '.flaskenv'))
 
 
 class Config(object):
+    # App Name
     APP_NAME = "SomeApp"
     
+    # Security settings
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this-secret-key'
-    SECURITY_PASSWORD_SALT = 'super-secret-salty-salt'
+    #SECURITY_PASSWORD_SALT = 'super-secret-salty-salt'
     
+    # SQLAlchemy Database settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    MAIL_DEFAULT_SENDER = 'smtp.someserver.com'
+    # Flask-Mail Email settings
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+    MAIL_DEBUG = False
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+    MAIL_DEFAULT_SENDER = 'someaddress@yourorg.co.uk'
+    MAIL_MAX_EMAILS = None
+    MAIL_SUPPRESS_SEND = False
+    MAIL_ASCII_ATTACHMENTS = False
